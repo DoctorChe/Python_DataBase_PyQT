59a33e2fddd1e32378591d9619147889ebe69b74
DoctorChe <duncan.reg@yandex.ru>
2019-09-03 00:05:09 +0300

    add message compression

--- client/utils/message.py
+++ client/utils/message.py
@@ -2,6 +2,7 @@ import json
 import zlib
 
 from client.utils.config_client import ENCODING, MSG_SIZE
+from client.utils.errors import MessageIsNotDictError
 
 
 def send_message(socket, msg: dict):
@@ -11,11 +12,12 @@ def send_message(socket, msg: dict):
     :param msg: словарь сообщения
     :return: None
     """
-    # js_message = json.dumps(msg)
-    # encoded_message = js_message.encode(ENCODING)
-    # compressed_message = zlib.compress(encoded_message)
-    # socket.send(compressed_message)
-    socket.send(json.dumps(msg).encode(ENCODING))
+    if not isinstance(msg, dict):
+        raise MessageIsNotDictError
+    js_message = json.dumps(msg)
+    encoded_message = js_message.encode(ENCODING)
+    compressed_message = zlib.compress(encoded_message)
+    socket.send(compressed_message)
 
 
 def recieve_message(socket) -> dict:
@@ -24,14 +26,11 @@ def recieve_message(socket) -> dict:
     :param socket: сокет
     :return: словарь сообщения
     """
-    # return json.loads(socket.recv(MSG_SIZE).decode(ENCODING))
     try:
-        # compressed_message = socket.recv(MSG_SIZE)
-        # decompressed_message = zlib.decompress(compressed_message)
-        # decoded_message = decompressed_message.decode(ENCODING)
-        # message = json.loads(decoded_message)
-        message = json.loads(socket.recv(MSG_SIZE).decode(ENCODING))
-        # return json.loads(socket.recv(MSG_SIZE).decode(ENCODING))
+        compressed_message = socket.recv(MSG_SIZE)
+        decompressed_message = zlib.decompress(compressed_message)
+        decoded_message = decompressed_message.decode(ENCODING)
+        message = json.loads(decoded_message)
     # except ConnectionAbortedError:
     #     return {
     #         "response": 499,
--- server/utils/message.py
+++ server/utils/message.py
@@ -1,21 +1,30 @@
 import json
+import zlib
+
 from server.utils.config_server import ENCODING, MSG_SIZE
 
 
-def send_message(socket, msg):
+def send_message(socket, msg: dict):
     """
     Отправка сообщения
     :param socket: сокет
     :param msg: словарь сообщения
     :return: None
     """
-    socket.send(json.dumps(msg).encode(ENCODING))
+    js_message = json.dumps(msg)
+    encoded_message = js_message.encode(ENCODING)
+    compressed_message = zlib.compress(encoded_message)
+    socket.send(compressed_message)
 
 
-def recieve_message(socket):
+def recieve_message(socket) -> dict:
     """
     Получение сообщения
     :param socket: сокет
     :return: словарь сообщения
     """
-    return json.loads(socket.recv(MSG_SIZE).decode(ENCODING))
+    compressed_message = socket.recv(MSG_SIZE)
+    decompressed_message = zlib.decompress(compressed_message)
+    decoded_message = decompressed_message.decode(ENCODING)
+    message = json.loads(decoded_message)
+    return message
