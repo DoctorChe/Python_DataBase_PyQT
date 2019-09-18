37e8889e997b2dc7664418b8f8832fc3c2633ea0
DoctorChe <duncan.reg@yandex.ru>
2019-09-18 17:05:11 +0300

    correct travis integration

--- setup.py
+++ setup.py
@@ -22,4 +22,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        "console_scripts": [
+            "server_messenger = server.__main__"
+        ]
+    }
 )
