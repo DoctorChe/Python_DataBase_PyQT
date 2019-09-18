b2fd58609336a6258db6187f5245b211f48a75bc
DoctorChe <duncan.reg@yandex.ru>
2019-09-18 16:33:18 +0300

    deploy

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+from server.utils.config_server import PROGRAM, VERSION
+
+DESCRIPTION = "Messenger (server part)"
+
+setup(
+    name=PROGRAM,
+    version=VERSION,
+    license="MIT License",
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    include_package_data=True,
+    url="https://github.com/DoctorChe/Python_DataBase_PyQT",
+    author="Doctor_Che",
+    author_email="duncan.reg@yandex.ru",
+    maintainer="Doctor_Che",
+    maintainer_email="duncan.reg@yandex.ru",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
